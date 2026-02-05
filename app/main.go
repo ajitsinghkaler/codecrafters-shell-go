036664ee85a6e1048ef9eb72756bae3e8998d59e
Ajit Singh <ajitsinghkaler0@gmail.com>
2026-02-05 13:09:32 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -23,97 +23,85 @@ func main() {
 		}
 
 		command = command[:len(command)-1]
-		builtIn := []string{"exit", "echo", "type", "pwd", "cd"}
-		if command == builtIn[0] {
+		tokens := tokenize(command)
+		arguements := strings.Join(tokens[1:], " ")
+		pathEnv := os.Getenv("PATH")
+		allPaths := strings.Split(pathEnv, string(os.PathListSeparator))
+
+		builtIn := []string{"exit", "echo", "pwd", "cd", "type"}
+
+		switch tokens[0] {
+		case builtIn[0]:
 			os.Exit(0)
-		}
-
-		if strings.HasPrefix(command, builtIn[1]) {
-			echoWords := command[len("echo "):]
-			results := clearArguements(echoWords)
-			fmt.Println(strings.Join(results, " "))
-			continue
-		}
-
-		if strings.HasPrefix(command, builtIn[4]) {
-			absolutePath := command[3:]
-			if absolutePath == "~" {
-				absolutePath = os.Getenv("HOME")
-			}
-
-			err := os.Chdir(absolutePath)
-			if err != nil {
-				fmt.Println("cd:", absolutePath+":", "No such file or directory")
-			}
-			continue
-		}
-
-		if command == builtIn[3] {
+		case builtIn[1]:
+			fmt.Println(arguements)
+		case builtIn[2]:
 			dir, err := os.Getwd()
 			if err != nil {
 				os.Exit(1)
 			}
 			fmt.Println(dir)
-			continue
-		}
-		pathEnv := os.Getenv("PATH")
-		allPaths := strings.Split(pathEnv, string(os.PathListSeparator))
-		if strings.HasPrefix(command, "cat ") {
-			paths := command[4:]
-			outputPaths := clearArguements(paths)
-			cmd := exec.Command("cat", outputPaths...)
-			output, err := cmd.Output()
-			if err != nil {
-				fmt.Print("An error occured while outputting")
+		case builtIn[3]:
+			if len(tokens) < 2 {
+				continue
 			}
+			path := tokens[1]
+			if path == "~" {
+				path = os.Getenv("HOME")
+			}
+
+			err := os.Chdir(path)
+			if err != nil {
+				fmt.Println("cd:", path+":", "No such file or directory")
+			}
+
+		case "cat":
+			cmd := exec.Command("cat", tokens[1:]...)
+			output, _ := cmd.CombinedOutput()
 			fmt.Print(string(output))
-			continue
-
-		}
-
-		if strings.HasPrefix(command, builtIn[2]) {
-			typeCommand := command[len("type "):]
-			if slices.Contains(builtIn, typeCommand) {
-				fmt.Println(typeCommand, "is a shell builtin")
+		case builtIn[4]:
+			typeArg := tokens[1]
+			if slices.Contains(builtIn, typeArg) {
+				fmt.Println(typeArg, "is a shell builtin")
 				continue
 			}
 			found := false
 			for _, path := range allPaths {
-				fullPath := fmt.Sprintf("%s/%s", path, typeCommand)
+				fullPath := fmt.Sprintf("%s/%s", path, typeArg)
 				if FileExecutableExists(fullPath) {
-					fmt.Println(typeCommand, "is", fullPath)
+					fmt.Println(typeArg, "is", fullPath)
 					found = true
 					break
 				}
 			}
 
 			if !found {
-				fmt.Print(typeCommand, ": not found \n")
+				fmt.Print(typeArg, ": not found \n")
 			}
-			continue
-		}
-		commandParts := strings.Split(command, " ")
-		fileExecutableName := commandParts[0]
+		default:
+			fileExecutableName := tokens[0]
 
-		executable := false
-		for _, path := range allPaths {
-			fullPath := fmt.Sprintf("%s/%s", path, fileExecutableName)
-			if FileExecutableExists(fullPath) {
-				cmd := exec.Command(fileExecutableName, commandParts[1:]...)
-				cmdOutput, err := cmd.CombinedOutput()
-				if err != nil {
-					os.Exit(1)
+			executable := false
+			for _, path := range allPaths {
+				fullPath := fmt.Sprintf("%s/%s", path, fileExecutableName)
+				if FileExecutableExists(fullPath) {
+					cmd := exec.Command(fileExecutableName, tokens[1:]...)
+					cmdOutput, err := cmd.CombinedOutput()
+					if err != nil {
+						os.Exit(1)
+					}
+					fmt.Print(string(cmdOutput))
+					executable = true
+					break
 				}
-				fmt.Print(string(cmdOutput))
-				executable = true
-				break
 			}
-		}
-		if executable {
-			continue
+			if executable {
+				continue
+			}
+
+			fmt.Print(command, ": command not found \n")
 		}
 
-		fmt.Print(command, ": command not found \n")
 	}
 }
 
@@ -127,17 +115,32 @@ func FileExecutableExists(filename string) bool {
 	return info.Mode().Perm()&0111 != 0
 }
 
-func clearArguements(echoWords string) []string {
+func tokenize(shellInput string) []string {
 	var results []string
 	finalWord := ""
 	inQuotes := false
 	inDoubleQuotes := false
-	for i := 0; i < len(echoWords); i++ {
-		char := echoWords[i]
-		if char == '\\' && i+1 < len(echoWords) && !inQuotes {
-			finalWord = finalWord + string(echoWords[i+1])
-			i++
-			continue
+	for i := 0; i < len(shellInput); i++ {
+		char := shellInput[i]
+		if char == '\\' {
+			if inDoubleQuotes {
+				if i+1 < len(shellInput) {
+					next := shellInput[i+1]
+					if next == '$' || next == '"' || next == '\\' || next == '`' || next == '\n' {
+						finalWord += string(next)
+						i++
+						continue
+					}
+				}
+				finalWord += string(char)
+				continue
+			} else if !inQuotes {
+				if i+1 < len(shellInput) {
+					finalWord += string(shellInput[i+1])
+					i++
+					continue
+				}
+			}
 		}
 
 		if char == '\'' && !inDoubleQuotes {
